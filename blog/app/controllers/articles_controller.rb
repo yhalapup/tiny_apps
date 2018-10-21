85ac8e7081818a0eb87bffccc60253d5d874f30b
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-21 13:12:18 +0300

    add update action

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -26,6 +26,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
